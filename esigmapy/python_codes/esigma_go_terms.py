fcf3f4e6c03add5c84b0b746ced5a8f91af27217
Prayush Kumar <prayush.kumar@gmail.com>
2026-06-19 00:44:57 +0530

    fixup: perf improvements

--- esigmapy/python_codes/esigma_go_terms.py
+++ esigmapy/python_codes/esigma_go_terms.py
@@ -6177,10 +6177,10 @@ def generate_hlm(
     amplitude = (4 * total_mass * eta * np.sqrt(M_PI / 5.0)) / R
 
     if m < 0:
-        amplitude = (-1) ** l * amplitude
+        if l & 1: amplitude = - amplitude
         waveform_modes = np.conjugate(waveform_modes)
 
-    phase_factor = rect(1.0, -m * Phi)
+    phase_factor = np.exp(1j * (- m * Phi))
     return amplitude * waveform_modes * phase_factor
 
 
