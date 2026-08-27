e3833a99fa090ad757d85e5a120da35b202663a1
Samanwaya Mukherjee <samanwaya.mukherjee@gmail.com>
2026-08-27 23:44:12 +0530

    Fix import names, add new functions for IMRESIGMA

--- esigmapy/__init__.py
+++ esigmapy/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from . import legacy, utils, condition, config
+from . import legacy, utils, condition, config, blend
 from .config import configure, get_config
 
 from .inspiral.lalsimulation_backend.generator import (
@@ -12,7 +12,7 @@ from .inspiral.lalsimulation_backend.generator import (
     get_inspiral_esigma_waveform,
 )
 
-from .inspiral import get_modes, get_dynamics, get_waveform
+from .inspiral import get_inspiral_modes, get_inspiral_dynamics, get_inspiral_waveform
 
 
 def get_version_information():
@@ -55,6 +55,60 @@ def pycbc_esigma(**params):
         pt.pop("delta_t"),
         **pt,
         modes_to_use=modes
+    )
+
+def get_imr_modes(**params):
+
+    # Pass all parameters the model suppports
+    pt = {p: params[p] for p in _params if p in params}
+    pt["f_ref"] = pt.get("f_ref") or pt["f_lower"]
+
+    backend = params.pop("backend","numba")
+
+    import importlib
+
+    module = importlib.import_module(
+        f"esigmapy.inspiral.{backend}_backend"
+    )
+
+    gen_wav = getattr(module, "get_imr_esigma_modes_py")
+
+    if "skip_merger" in params:
+        gen_wav = get_inspiral_esigma_waveform
+
+    return gen_wav(
+        pt.pop("mass1"),
+        pt.pop("mass2"),
+        pt.pop("f_lower"),
+        pt.pop("delta_t"),
+        **pt,
+    )
+
+def get_imr_waveform(**params):
+
+    # Pass all parameters the model suppports
+    pt = {p: params[p] for p in _params if p in params}
+    pt["f_ref"] = pt.get("f_ref") or pt["f_lower"]
+
+    backend = params.pop("backend","numba")
+
+    import importlib
+
+    module = importlib.import_module(
+        f"esigmapy.inspiral.{backend}_backend"
+    )
+
+    gen_wav = getattr(module, "get_imr_esigma_waveform_py")
+
+    if "skip_merger" in params:
+        gen_wav = get_inspiral_esigma_waveform
+
+    return gen_wav(
+        pt.pop("mass1"),
+        pt.pop("mass2"),
+        pt.pop("f_lower"),
+        pt.pop("delta_t"),
+        **pt,
     )
 
 
